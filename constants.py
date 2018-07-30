0dd3f1357045e97c1671bc90a531c61d1261ca89
belee325 <belee325@gmail.com>
2018-07-30 19:41:16 -0400

    removed duplicates and pw

--- constants.py
+++ constants.py
@@ -1,11 +1,11 @@
-CONSUMER_KEY='8w1EAc8IcH4KUqCCHNk2DCj5D'
-CONSUMER_SECRET='UsRRWlPtPqe5a13Oyw2Ln5QSFpBtdTlD6mhUzo9mwW1RYvFfXt'
+CONSUMER_KEY=''
+CONSUMER_SECRET=''
 
 REQUEST_TOKEN_URL='https://api.twitter.com/oauth/request_token'
 ACCESS_TOKEN_URL='https://api.twitter.com/oauth/access_token'
 AUTHORIZATION_URL='https://api.twitter.com/oauth/authorize'
 
-DATABASE = 'learn'
-USER = 'postgres'
-PASSWORD = 'reversevengence353'
-HOST = 'localhost'
+DATABASE = ''
+USER = ''
+PASSWORD = ''
+HOST = ''
